0751360cabb86e87a3f091abdc0eb5ed5d2cea97
nguyenxuanhung23 <97092413+nguyenxuanhung23@users.noreply.github.com>
2022-02-25 00:19:51 +0700

    update logout, changepassword

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,13 +23,13 @@
     <!-- END: Vendor CSS-->
 
     <!-- BEGIN: Theme CSS-->
-{{--    <link rel="stylesheet" type="text/css" href="/app-assets/css/bootstrap.css">--}}
-{{--    <link rel="stylesheet" type="text/css" href="/app-assets/css/bootstrap-extended.css">--}}
-{{--    <link rel="stylesheet" type="text/css" href="/app-assets/css/colors.css">--}}
-{{--    <link rel="stylesheet" type="text/css" href="/app-assets/css/components.css">--}}
-{{--    <link rel="stylesheet" type="text/css" href="/app-assets/css/themes/dark-layout.css">--}}
-{{--    <link rel="stylesheet" type="text/css" href="/app-assets/css/themes/bordered-layout.css">--}}
-{{--    <link rel="stylesheet" type="text/css" href="/app-assets/css/themes/semi-dark-layout.css">--}}
+    <link rel="stylesheet" type="text/css" href="/app-assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="/app-assets/css/bootstrap-extended.css">
+    <link rel="stylesheet" type="text/css" href="/app-assets/css/colors.css">
+    <link rel="stylesheet" type="text/css" href="/app-assets/css/components.css">
+    <link rel="stylesheet" type="text/css" href="/app-assets/css/themes/dark-layout.css">
+    <link rel="stylesheet" type="text/css" href="/app-assets/css/themes/bordered-layout.css">
+    <link rel="stylesheet" type="text/css" href="/app-assets/css/themes/semi-dark-layout.css">
 
     <!-- BEGIN: Page CSS-->
     <link rel="stylesheet" type="text/css" href="/app-assets/css/core/menu/menu-types/horizontal-menu.css">
@@ -285,7 +285,7 @@
                     <div class="user-nav d-sm-flex d-none"><span class="user-name fw-bolder">John Doe</span><span class="user-status">Admin</span></div><span class="avatar"><img class="round" src="/app-assets//images/portrait/small/avatar-s-11.jpg" alt="avatar" height="40" width="40"><span class="avatar-status-online"></span></span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdown-user"><a class="dropdown-item" href="page-profile.html"><i class="me-50" data-feather="user"></i> Profile</a><a class="dropdown-item" href="app-email.html"><i class="me-50" data-feather="mail"></i> Inbox</a><a class="dropdown-item" href="app-todo.html"><i class="me-50" data-feather="check-square"></i> Task</a><a class="dropdown-item" href="app-chat.html"><i class="me-50" data-feather="message-square"></i> Chats</a>
-                    <div class="dropdown-divider"></div><a class="dropdown-item" href="page-account-settings-account.html"><i class="me-50" data-feather="settings"></i> Settings</a><a class="dropdown-item" href="page-pricing.html"><i class="me-50" data-feather="credit-card"></i> Pricing</a><a class="dropdown-item" href="page-faq.html"><i class="me-50" data-feather="help-circle"></i> FAQ</a><a class="dropdown-item" href="auth-login-cover.html"><i class="me-50" data-feather="power"></i> Logout</a>
+                    <div class="dropdown-divider"></div><a class="dropdown-item" href="page-account-settings-account.html"><i class="me-50" data-feather="settings"></i> Settings</a><a class="dropdown-item" href="page-pricing.html"><i class="me-50" data-feather="credit-card"></i> Pricing</a><a class="dropdown-item" href="page-faq.html"><i class="me-50" data-feather="help-circle"></i> FAQ</a><a class="dropdown-item" href="{{route('logout')}}"><i class="me-50" data-feather="power"></i> Đăng xuất</a>
                 </div>
             </li>
         </ul>
